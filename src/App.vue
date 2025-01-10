b388c8e1b4a619e13b2e34f9757b8a00c1568dd9
Anderdann <ANDERDANN@GMAIL.COM>
2025-01-10 18:12:42 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@ const accept = () => {
   <div v-if="step === 2" class="container">
     <h2>Sim, eu sabia que estava... hahaha!</h2>
     <h3>Agora chama o seu fedido favorito, fale alguma coisa que tire ele do centro, eu duvido!!! 🥰</h3>
-
+    <a href="https://wa.me/5515996308585" target="_blank">Fale comigo baby</a>
   </div>
 </template>
 
